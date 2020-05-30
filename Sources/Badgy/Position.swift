d403e9a65638ed4541f8c04efd039dbe8ac89196
Arthur Alves <arthura@backbase.com>
2020-05-30 17:17:08 +0200

    chore: move Position enum to its own file

---
+++ Sources/Badgy/Position.swift
@@ -0,0 +1,37 @@
+//
+//  Badgy
+//
+//  Created by Arthur Alves on 30/05/2020.
+//
+
+import Foundation
+
+enum Position: String {
+    case top, left, bottom, right
+    case topLeft, topRight
+    case bottomLeft, bottomRight
+    case center
+    
+    var cardinal: String {
+        switch self {
+        case .top:
+            return "North"
+        case .left:
+            return "West"
+        case .bottom:
+            return "South"
+        case .right:
+            return "East"
+        case .topLeft:
+            return "Northwest"
+        case .topRight:
+            return "Northeast"
+        case .bottomLeft:
+            return "Southwest"
+        case .bottomRight:
+            return "Southeast"
+        case .center:
+            return "Center"
+        }
+    }
+}
